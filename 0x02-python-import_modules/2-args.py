5603863b62688d2818b4bd2b9e0bf4a6a64faafe
Edwin uzoma <fuzibibi@gmail.com>
2023-01-10 12:36:54 -0800

    my third directory

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-def arguments(args):
-if len(args) <= 1:
-print("0 arguments.")
-else:
-if (len(args)) == 2:
-print(len(args) - 1, "argument:")
-else:
-print(len(args) - 1, "arguments:")
-for i, a in enumerate(args[1:], start=1):
-print("{}: {}".format(i, a))
 if __name__ == "__main__":
 import sys
-arguments(sys.argv)
+argv = sys.argv[1:]
+argv_count = len(argv)
+index = 1
+if argv_count is 0:
+print("{:d} arguments.".format(argv_count))
+elif argv_count is 1:
+print("{:d} argument:".format(argv_count))
+print("{:d}: {:s}".format(index, sys.argv[1]))
+else:
+print("{:d} arguments:".format(argv_count))
+while index <= argv_count:
+print("{:d}: {:s}".format(index, sys.argv[index]))
+index += 1
