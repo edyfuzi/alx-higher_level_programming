2e5cd07bc3355b10ae5d6650a157c49cd94dcf7d
Edwin uzoma <fuzibibi@gmail.com>
2023-01-10 13:31:59 -0800

    my third directory

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def main(*argv):
+1 = len(sys.argv)
+sum = 0
+if l > 1:
+for args in sys.argv:
+if args != sys.argv[0]:
+sum = sum + int(args)
+print(sum)
+if __name__ == "__main__":
+main()
